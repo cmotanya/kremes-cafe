7a99f5d68ce8bb77be330dad26fb5e463d8f10f9
Cornelius Motanya <corneliusmot@yahoo.com>
2024-12-31 10:19:03 +0300

    first commit

---
+++ app/components/image-slider.tsx
@@ -0,0 +1,161 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { cn } from "../utils/cn";
+import { AnimatePresence, motion } from "framer-motion";
+
+export const ImageSlider = ({
+  images,
+  children,
+  overlay = true,
+  overlayClassName,
+  className,
+  autoplay = true,
+  direction = "up",
+}: {
+  images: string[];
+  children: React.ReactNode;
+  overlay?: React.ReactNode;
+  overlayClassName?: string;
+  className?: string;
+  autoplay?: boolean;
+  direction?: "up" | "down";
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [, setLoading] = useState(true);
+  const [loadedImages, setLoadedImages] = useState<string[]>([]);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex + 1 === images.length ? 0 : prevIndex + 1,
+    );
+  };
+
+  const handlePrevious = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1,
+    );
+  };
+
+  useEffect(() => {
+    //   Store cleanup function
+    let mounted = true;
+    const loadedImages = async () => {
+      setLoading(true);
+
+      const loadedPromises = images.map((image) => {
+        return new Promise((resolve, reject) => {
+          const img = new Image();
+          img.src = image;
+          img.onload = () => resolve(image);
+          img.onerror = () => reject(image);
+        });
+      });
+
+      try {
+        const results = await Promise.all(loadedPromises);
+        if (mounted) {
+          setLoadedImages(results as string[]);
+          setLoading(false);
+        }
+      } catch (error) {
+        console.error("Error loading images", error);
+        if (mounted) {
+          setLoading(false);
+        }
+      }
+    };
+
+    loadedImages();
+
+    //   Cleanup function
+    return () => {
+      mounted = false;
+    };
+  }, [images]);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "ArrowRight") {
+        handleNext();
+      } else if (e.key === "ArrowLeft") {
+        handlePrevious();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    //   autoplay
+    let interval: NodeJS.Timeout;
+    if (autoplay) {
+      interval = setInterval(() => {
+        handleNext();
+      }, 5000);
+    }
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      clearInterval(interval);
+    };
+  });
+
+  const slideVariants = {
+    initial: {
+      opacity: 0,
+      scale: 0,
+      rotateX: 45,
+    },
+    visible: {
+      scale: 1,
+      opacity: 1,
+      rotateX: 0,
+      transition: {
+        duration: 0.5,
+        ease: [0.645, 0.045, 0.355, 1.0],
+      },
+    },
+    upExit: {
+      opacity: 1,
+      y: "150%",
+      transition: { duration: 1 },
+    },
+    downExit: {
+      opacity: 1,
+      y: "150%",
+      transition: { duration: 1 },
+    },
+  };
+
+  const areImagesLoaded = loadedImages.length > 0;
+
+  return (
+    <div
+      className={cn(
+        "relative flex h-full items-center justify-center overflow-hidden",
+        className,
+      )}
+      style={{ perspective: "1000px" }}
+    >
+      {areImagesLoaded && children}
+      {areImagesLoaded && overlay && (
+        <div
+          className={cn("absolute inset-0 z-40 bg-black/60", overlayClassName)}
+        />
+      )}
+
+      {areImagesLoaded && (
+        <AnimatePresence>
+          <motion.img
+            key={currentIndex}
+            src={loadedImages[currentIndex]}
+            initial="initial"
+            animate="visible"
+            exit={direction === "up" ? "upExit" : "downExit"}
+            variants={slideVariants}
+            className="absolute inset-0 h-full w-full object-cover object-center"
+          ></motion.img>
+        </AnimatePresence>
+      )}
+    </div>
+  );
+};
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Intro from "./intro";
+
+export default function Home() {
+  return (
+    <main>
+      <Intro />
+    </main>
+  );
+}
