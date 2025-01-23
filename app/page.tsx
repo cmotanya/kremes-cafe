16d533916219e42dc25ef021f0894aaf4c9697c5
Cornelius Motanya <corneliusmot@yahoo.com>
2025-01-23 21:30:47 +0300

    changed to use client

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import OrderPage from "./pages/orderPage";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/order" element={<OrderPage />} />
-      </Routes>
-    </Router>
+    <>
+      <HomePage />
+      <OrderPage />
+    </>
   );
 }
